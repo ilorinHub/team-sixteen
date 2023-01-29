d0d0aa6c681ec147a99c9d5b3b7c36453395c728
Abdulgafar <tajudeenabdulgafar4@gmail.com>
2023-01-29 17:01:20 +0100

    new commit

--- src/pages/LandingPage/Footer.js
+++ src/pages/LandingPage/Footer.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Logo from "../../images/FooterLogo.svg";
+import FacebookImg from "../../images/FacebookImg.svg";
+import LinkedInImg from "../../images/LinkedInImg.svg";
+import TwitterImg from "../../images/TwitterImg.svg";
+
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <img src={Logo} alt="" />
+      <ul className="footerUl firstChild">
+        <li>
+          <a href="#pricing">
+            <img src={FacebookImg} alt="" />
+          </a>
+        </li>
+
+        <li>
+          <a href="#pricing">
+            <img src={LinkedInImg} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#pricing">
+            <img src={TwitterImg} alt="" />
+          </a>
+        </li>
+      </ul>
+      <ul className="footerUl">
+        <li>
+          <a href="#pricing">Pricing</a>
+        </li>
+
+        <li>
+          <a href="#faqs">FAQs</a>
+        </li>
+        <li>
+          <a href="#patners">Patners</a>
+        </li>
+        <li>
+          <a href="#career">Career</a>
+        </li>
+        <li>
+          <a href="#resources">Resources</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
+      </ul>
+      <p>© Copyright 2023. MedCloud Technologies Pvt. Ltd</p>
+    </div>
+  );
 }
 
 export default Footer
